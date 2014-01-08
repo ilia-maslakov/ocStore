50a5ec7ce49c77f8fb594fe3e8a24f2322a160af
Ilia Maslakov <il.smind@gmail.com>
2014-01-08 15:27:26 +0300

    Добавлен доступ к опциям товара в category.php
    для доступа из представления категорий изменён контроллер ControllerProductCategory. Теперь обойти массив опций можно в цикле.

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -241,11 +241,14 @@ class ControllerProductCategory extends Controller {
 					$rating = false;
 				}
 
+				$options = $this->model_catalog_product->getProductOptions($result['product_id']);
+
 				$this->data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_list_description_limit')) . '..',
+					'options'     => $options,
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
